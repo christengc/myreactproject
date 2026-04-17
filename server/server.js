767c3f0e532d2af73f93606f623f1f0f3ab58543
Christen Christensen <christenchristensen@live.dk>
2026-04-17 07:47:16 +0200

    change file size limit

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ app.use(cors({
   origin: ['https://www.christenchristensen.dk', 'https://christenchristensen.dk']
 }));
 
-app.use(express.json({ limit: '5mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/api', mailRoutes);
 app.use('/api', golfTrackingRoutes);
 app.use('/',healthRoutes);
